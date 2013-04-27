cf8a4a4e6ab06ba388a30873352c2d5acbe98a2a
Craig Christenson <christensoncraig@gmail.com>
2013-04-27 10:07:27 -0400

    Adding direct checkout form

--- twocheckout/charge.py
+++ twocheckout/charge.py
@@ -23,6 +23,16 @@ class Charge:
                "<script type=\"text/javascript\">document.getElementById('2checkout').submit();</script>"
 
     @classmethod
+    def direct(cls, params=None):
+        if params is None:
+            params = dict()
+        form = "<form id=\"2checkout\" action=\"https://www.2checkout.com/checkout/purchase\" method=\"post\">\n"
+        for param in params:
+            form = form + "<input type=\"hidden\" name=\"" + param + "\" value=\"" + str(params[param]) + "\" />\n"
+        return form + "<input type=\"submit\" value=\"Proceed to Checkout\" />\n</form>\n" + \
+               "<script src=\"https://www.2checkout.com/static/checkout/javascript/direct.min.js\"></script>"
+
+    @classmethod
     def link(cls, params=None, url="https://www.2checkout.com/checkout/spurchase?"):
         if params is None:
             params = dict()
